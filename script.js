1beb2945a745acc119484d8025b2169d12121b9d
SawSithu <saws82961@gmail.com>
2024-12-29 10:17:14 +0630

    Nav bar finished plus index page modified

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-var nav = document.getElementById("nav");
+const bars = document.getElementById("bars");
+const nitems =document.getElementById("nitems");
+
+function test() {
+    console.log("clicked")
+}
+bars.addEventListener("click", () => {
+    bars.classList.toggle("active");
+    nitems.classList.toggle("come");
+});
+
