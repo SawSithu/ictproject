712ea06fbdb6697b78606c528aa63cbb69e7ab97
Hlaing Khant Aung <86865944+HlaingKhantAung001@users.noreply.github.com>
2024-12-30 13:29:19 +0630

    Binded Go to Game Button
    Go to game Button is added

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-var nav = document.getElementById("nav");
+const bars = document.getElementById("bars");
+const nitems =document.getElementById("nitems");
+function test() {
+    console.log("clicked")
+}
+bars.addEventListener("click", () => {
+    bars.classList.toggle("active");
+    nitems.classList.toggle("come");
+});
+
+// ------------------------------ Three ------------------------------
+
+
